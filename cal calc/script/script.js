b5d53d6d0a41dec2e62ca8ec26f6d4756b04d919
Zhdanov Vadim <101713038+ScreamSine@users.noreply.github.com>
2022-05-09 18:15:10 +0200

    second commit - add css style and begining js code

--- cal calc/script/script.js
+++ cal calc/script/script.js
@@ -1,1 +1,17 @@
+const userDataValue = document.querySelectorAll(".entervalue");
+const calcButton = document.querySelector(".calculate");
+const allClear = document.querySelector(".allClear");
+const paragraph = document.querySelector(".testing");
 
+userDataValue.forEach((div) => {
+  calcButton.addEventListener("click", function () {
+    paragraph.innerHTML =
+      parseInt(userDataValue[0].value) +
+      parseInt(userDataValue[1].value) +
+      parseInt(userDataValue[2].value);
+  });
+});
+
+allClear.addEventListener("click", function () {
+  paragraph.innerHTML = "";
+});
